e4497eb44a22d4905a045d6e2d08904d5f302e99
Rasmus <14540163+rmvermeulen@users.noreply.github.com>
2022-12-25 23:11:57 +0100

    add missing types and test cmd

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -1,5 +1,13 @@
+import { Dictionary } from "rambda";
 import { RouteConfig, RouteMap, RouteMapValue } from "./types";
 export declare const placeholderRegExp: RegExp;
 export declare const getMatchableRoutes: (routes: RouteMap) => string[];
+/**
+ * A low-level variable matcher that does no type checking or conversions
+ * @param pattern a route pattern with optional variables
+ * @param route a route as a absolute or relative url
+ * @returns a map of variables
+ */
+export declare const getRouteVariables: (pattern: string, route: string) => Dictionary<string>;
 export declare const getMatches: (route: string, routeMap: RouteMap) => (RegExpExecArray | null)[];
 export declare const isRouteConfig: (value: RouteMapValue) => value is RouteConfig<any, any>;
